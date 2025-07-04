9d47f64a747ee687db2047f7c17cfedf25b8355a
tirthankar95 <tirthankarmittra@gmail.com>
2025-07-04 15:23:13 +0000

    Add delta change

--- verl/trainer/ppo/ray_trainer.py
+++ verl/trainer/ppo/ray_trainer.py
@@ -489,7 +489,7 @@ class RayPPOTrainer:
             dataset=self.train_dataset,
             batch_size=self.config.data.get("gen_batch_size", self.config.data.train_batch_size),
             num_workers=self.config.data.get("dataloader_num_workers", 8),
-            drop_last=True,
+            drop_last=False,
             collate_fn=collate_fn,
             sampler=train_sampler,
         )
