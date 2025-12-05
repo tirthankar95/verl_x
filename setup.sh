2a53216cee2d4602e4c68d2afc524a2fd24c29af
tirthankar95 <tirthankarmittra@gmail.com>
2025-12-05 22:21:06 +0530

    Change reward function.

--- setup.sh
+++ setup.sh
@@ -6,5 +6,5 @@ uv venv ../ai --python=python3.12.3
 source ~/ai/bin/activate
 uv pip install -r requirements.txt
 uv pip install flash-attn==2.8.0.post2 --no-build-isolation
-python3 recipe/grid_dapo/prepare_data.py & 
+python3 recipe/grid_dapo/prepare_data.py --style relax
 python3 get_models.py
