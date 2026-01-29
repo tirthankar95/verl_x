285c89b0e96cb476fe77142e811eaf9171ce0350
tirthankar95 <tirthankarmittra@gmail.com>
2026-01-29 12:07:14 +0000

    Fix get the correct model

--- get_models.py
+++ get_models.py
@@ -1,25 +1,20 @@
 import os
-import json
-from transformers import AutoTokenizer, AutoModelForCausalLM
+
 from huggingface_hub import hf_hub_download, list_repo_files
 
-my_token = os.getenv('HF_TOKEN', '')
-home = os.path.expanduser('~')
+my_token = os.getenv("HF_TOKEN", "")
+home = os.path.expanduser("~")
+
 
 def get_model(repo_id, local_dir):
     filenames = list_repo_files(repo_id)
     for filename in filenames:
-        print(hf_hub_download(repo_id = repo_id, \
-                              local_dir = local_dir, \
-                              token = my_token,
-                              filename=f"{filename}"))
+        print(hf_hub_download(repo_id=repo_id, local_dir=local_dir, token=my_token, filename=f"{filename}"))
 
-if __name__ == '__main__':
-    config = {
-        "repo_id": "Qwen/Qwen2-1.5B-Instruct",
-        "local_dir" : f"{home}/models/Qwen2-1.5B-Instruct"
-    }
-    '''
+
+if __name__ == "__main__":
+    config = {"repo_id": "Qwen/Qwen2.5-1.5B-Instruct", "local_dir": f"{home}/models/Qwen2-1.5B-Instruct"}
+    """
     config = {
         "repo_id": "Qwen/Qwen3-4B",
         "local_dir" : f"{home}/models/Qwen3-4B"
@@ -28,6 +23,5 @@ if __name__ == '__main__':
         "repo_id": "openai-community/gpt2",
         "local_dir": f"{home}/models/gpt2"
     }
-    '''
-    get_model(repo_id = config['repo_id'], \
-              local_dir = config['local_dir'])
+    """
+    get_model(repo_id=config["repo_id"], local_dir=config["local_dir"])
